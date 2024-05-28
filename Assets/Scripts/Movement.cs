219b872e01653b186f262306b37b767a64d823a8
yxoflorox <yxoflorox@gaming.tech>
2024-05-28 09:55:20 +0200

    test branche Flavien

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -7,7 +7,8 @@ public class Movement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        x = true;
+
     }
 
     // Update is called once per frame
