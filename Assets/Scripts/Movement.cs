216f520deb70606865d10f3e7fab53f5e1943321
Chotaki <116464680+Chotaki@users.noreply.github.com>
2024-05-28 02:45:11 +0200

    ft : move left & right

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float speed;
+    private float move;
+    private Rigidbody2D rb;
+
+    private void Start()
     {
-        
+        // On récupère le rigidBody de notre joueur
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // On rècupère l'input qui nous signal si on va à gauche ou à doite
+        move = Input.GetAxis("Horizontal");
+
+        // On change la velocité du rigibBody du joueur pour se déplacer
+        rb.velocity = new Vector2(move * speed, rb.velocity.y);
     }
 }
