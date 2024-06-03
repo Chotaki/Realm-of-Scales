ed9edc44a6521cc2d64ab16612f23fb9e72abad3
Chotaki <116464680+Chotaki@users.noreply.github.com>
2024-06-03 17:08:14 +0200

    ft : dash & crouch

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
 using Unity.Burst.CompilerServices;
 using UnityEngine;
 
@@ -12,10 +13,32 @@ public class Movement : MonoBehaviour
     private float _move;
     private Rigidbody2D _rb;
 
+    public Vector3 moveDirection;
+
+    private bool _canDash = true;
+    private bool _isDashing;
+    private float _dashingPower = 24f;
+    private float _dashingTime = 0.2f;
+    private float _dashingCooldown = 1f;
+
+    public SpriteRenderer spriteRenderer;
+    public Sprite crouching;
+    public Sprite standing;
+    public BoxCollider2D boxCollider;
+    public Vector2 standingSize;
+    public Vector2 crouchingSize;
+
+    [SerializeField] private TrailRenderer _trailRenderer;
+
     private void Start()
     {
         _isFloored = false;
         _rb = GetComponent<Rigidbody2D>();
+
+        boxCollider = GetComponent<BoxCollider2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = standing;
+        standingSize = boxCollider.size;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -33,16 +56,23 @@ public class Movement : MonoBehaviour
 
     void Update()
     {
-        _move = Input.GetAxis("Horizontal");
+        if (_isDashing)
+        {
+            return;
+        }
+
+        _move = Input.GetAxis("Horizontal"); // Q & D
 
         _rb.velocity = new Vector2(_move * speed, _rb.velocity.y);
 
-        if (_isFloored && !Input.GetButton("Jump"))
+        // Jump
+        if (_isFloored && !Input.GetButton("Jump")) // Space
         {
             _doubleJump = false;
         }
 
-        if (Input.GetButtonDown("Jump"))
+        // Double Jump
+        if (Input.GetButtonDown("Jump")) // Space
         {
             if (_isFloored || _doubleJump)
             {
@@ -51,13 +81,48 @@ public class Movement : MonoBehaviour
             }
         }
 
-        if (Input.GetButtonDown("Sprint"))
+        // Sprint
+        if (Input.GetButtonDown("Sprint")) // Right Mouse Button
         {
             speed += 10;
         }
-        if (Input.GetButtonUp("Sprint"))
+        if (Input.GetButtonUp("Sprint")) // Right Mouse Button
         {
             speed -= 10;
         }
+
+        // Dash
+        if (Input.GetButtonDown("Dash")) // R
+        {
+            StartCoroutine(Dash());
+        }
+
+        // Crouch
+        if (Input.GetButtonDown("Crouch")) // C
+        {
+            spriteRenderer.sprite = crouching;
+            boxCollider.size = crouchingSize;
+        }
+        if (Input.GetButtonUp("Crouch")) // C
+        {
+            spriteRenderer.sprite = standing;
+            boxCollider.size = standingSize;
+        }
+    }
+
+    private IEnumerator Dash()
+    {
+        _canDash = false;
+        _isDashing = true;
+        float originalGravity = _rb.gravityScale;
+        _rb.gravityScale = 0f;
+        _rb.velocity = new Vector2(_move * _dashingPower, 0f);
+        _trailRenderer.emitting = true;
+        yield return new WaitForSeconds(_dashingTime);
+        _trailRenderer.emitting = false;
+        _rb.gravityScale = originalGravity;
+        _isDashing = false;
+        yield return new WaitForSeconds(_dashingCooldown);
+        _canDash = true;
     }
 }
