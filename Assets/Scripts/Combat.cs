0b7895a926629798fdac338882fd0ef75620ad94
Chotaki <116464680+Chotaki@users.noreply.github.com>
2024-06-06 16:52:00 +0200

    ft :magie

--- Assets/Scripts/Combat.cs
+++ Assets/Scripts/Combat.cs
@@ -5,12 +5,18 @@ using UnityEngine;
 public class Combat : MonoBehaviour
 {
     public Transform attackPoint;
-    public float attackRange = 0.5f;
-    public int attackDamage = 20;
-    public float attackRate = 2f;
+
+    public float normalAttackRange = 0.5f;
+    public int normalAttackDamage = 20;
+
+    /*public float spellRange;
+    public int spellDamage;*/
+
+    private float _attackRate = 2f;
     private float _nextAttackTime = 0f;
 
     public LayerMask enemyLayer;
+    //private Collider2D[] _hitEnemies;
 
     void Update()
     {
@@ -18,27 +24,43 @@ public class Combat : MonoBehaviour
         {
             if (Input.GetKeyDown(KeyCode.Mouse0))
             {
-                attack();
-                _nextAttackTime = Time.time + 1f / attackRate;
+                normalAttack();
+                _nextAttackTime = Time.time + 1f / _attackRate;
             }
+            /*if (Input.GetKeyDown(KeyCode.E))
+            {
+                spell();
+                _nextAttackTime = Time.time + 1f / _attackRate;
+            }*/
         }
     }
 
-    public void attack()
+    public void normalAttack()
     {
-        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayer);
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, normalAttackRange, enemyLayer);
 
         foreach (Collider2D enemy in hitEnemies)
         {
-            enemy.GetComponent<Enemy>().takeDamage(attackDamage);
+            enemy.GetComponent<Enemy>().takeDamage(normalAttackDamage);
         }
     }
+
+/*    public void spell()
+    {
+        _hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, spellRange, enemyLayer);
+
+        foreach (Collider2D hitEnemy in _hitEnemies)
+        {
+            hitEnemy.GetComponent<Enemy>().takeDamage(spellDamage);
+        }
+    }*/
 
     private void OnDrawGizmosSelected()
     {
         if (attackPoint == null)
             return;
 
-        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+        Gizmos.DrawWireSphere(attackPoint.position, normalAttackRange);
+        //Gizmos.DrawWireSphere(attackPoint.position, spellRange);
     }
 }
