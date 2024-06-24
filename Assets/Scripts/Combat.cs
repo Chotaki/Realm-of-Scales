29e5a05dc4cbb7311a01586eb8b4603ecfd2e9b9
Chotaki <116464680+Chotaki@users.noreply.github.com>
2024-06-24 18:30:20 +0200

    Ft : potion & grab item + start charged attacks

--- Assets/Scripts/Combat.cs
+++ Assets/Scripts/Combat.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class Combat : MonoBehaviour
 {
@@ -8,8 +9,7 @@ public class Combat : MonoBehaviour
 
     public float normalAttackRange = 0.5f;
     public int normalAttackDamage = 20;
-
-    public int mana = 100;
+    public int chargedAttackDamage = 30;
 
     [SerializeField] public GameObject fireBall;
     public int spellDamage = 40;
@@ -17,6 +17,11 @@ public class Combat : MonoBehaviour
 
     private float _attackRate = 2f;
     private float _nextAttackTime = 0f;
+    private float _attackTimer;
+    private float _chargingTime = 2f;
+
+    public int mana = 100;
+    public int hp = 10;
 
     public LayerMask enemyLayer;
 
@@ -26,8 +31,26 @@ public class Combat : MonoBehaviour
         {
             if (Input.GetKeyDown(KeyCode.Mouse0))
             {
-                normalAttack();
-                _nextAttackTime = Time.time + 1f / _attackRate;
+                _attackTimer = Time.time;
+
+                if (Input.GetKeyUp(KeyCode.Mouse0))
+                {
+                    if (Time.time - _attackTimer > _chargingTime)
+                    {
+                        Debug.Log(_attackTimer);
+                        chargedAttack();
+                        _nextAttackTime = Time.time + 1f / _attackRate;
+                    } else
+                    {
+                        normalAttack();
+                        _nextAttackTime = Time.time + 1f / _attackRate;
+                    }
+                }
+                /*else
+                {
+                    normalAttack();
+                    _nextAttackTime = Time.time + 1f / _attackRate;
+                }*/
             }
             if (Input.GetKeyDown(KeyCode.E) && mana > 0)
             {
@@ -53,6 +76,16 @@ public class Combat : MonoBehaviour
         foreach (Collider2D enemy in hitEnemies)
         {
             enemy.GetComponent<Enemy>().takeDamage(normalAttackDamage);
+        }
+    }
+
+    public void chargedAttack()
+    {
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, normalAttackRange, enemyLayer);
+
+        foreach (Collider2D enemy in hitEnemies)
+        {
+            enemy.GetComponent<Enemy>().takeDamage(chargedAttackDamage);
         }
     }
 
---
+++ Assets/Scripts/Potion.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Potion : MonoBehaviour
+{
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+/*        for (int i = 0; i < player.GetComponent<Inventory>().inventory.Count; i++)
+        {*/
+            if (Input.GetKeyDown(KeyCode.U) /*&& player.GetComponent<Inventory>().inventory[i] == this*/)
+            {
+                player.GetComponent<Combat>().hp += 10;
+                player.GetComponent<Inventory>().inventory.Remove(this.gameObject);
+                Destroy(this.gameObject);
+            }
+        //}  
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject == player)
+        {
+            player.GetComponent<Inventory>().inventory.Add(this.gameObject);
+        }
+
+        this.gameObject.SetActive(false);
+    }
+}
